7416cf714a5f7fc1148c19faa23d179c77ff973a
angel lacret <alacret@gmail.com>
2015-03-14 17:54:54 +0000

    dummy commit

--- __openerp__.py
+++ __openerp__.py
@@ -10,13 +10,13 @@
         Long description of module's purpose
     """,
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
+    'author': "4geeks",
+    'website': "http://www.4geeks.co",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': '4geeks',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
